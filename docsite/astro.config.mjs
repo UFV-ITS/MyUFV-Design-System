fbf4c728d8eda5210ff98a459236c1c1fcb12b1b
Mykal Machon <Mykal.Machon@ufv.ca>
2023-06-09 11:17:40 -0700

    feat: image optimization
    images are now converted to webp on build, should be appropriately sized as well

--- docsite/astro.config.mjs
+++ docsite/astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
+  build: {
+    inlineStylesheets: "auto",
+  },
   integrations: [preact()]
 });
